0f4ed50bb9369147b7e94fddf82906fa3c4a2d1b
Kunkanya Remmer <kunkanyaremmer@hotmail.com>
2022-05-09 15:24:40 +0200

    V8: correction apartment page

--- src/page/Apartment/index.jsx
+++ src/page/Apartment/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment } from 'react'
 import Logements from '../../asset/logements.json'
 import { useParams } from 'react-router-dom'
 import './Apartment.css'
@@ -6,31 +6,34 @@ import Tag from '../../component/Tag'
 import Star from '../../component/Star'
 import AboutDropdown from '../../component/Dropdown'
 import Carousel from '../../component/Carousel'
+import Error404 from '../Error404'
 
-const Apartment = (props) => {
+const Apartment = () => {
   const { id } = useParams() 
-  const [isId] = useState("true")
-  const location = window.location.href 
-
-
+  let location = window.location.href 
+  let checkId = location.substr(32)
+  
+  let apartmentData =   Logements.find(logement => logement.id === checkId )
   
   return (
-    
+  /**Check if parameter {id} is the same as checkId from window.location.href 
+   * if ApartmentData is undefined and checkId isn´t the same as {id} so show the
+   * 404 page otherwise who the apartment page with data from apartmentData.
+  */
+  apartmentData === undefined && checkId !== id ? <Error404 /> : 
     <Fragment>
       <section className="apartment-wrapper">
-        {Logements.map((item) => {
-          return !(item.id === id) ? null : (
-            <section key={item.id} className="apartment-container">
-
-              <Carousel imageArr = {item.pictures} />
+            <section key={apartmentData.id} className="apartment-container">
+              {/**Add gallery carousel component  */}
+              <Carousel imageArr = {apartmentData.pictures} />
               
               <section className="apartment-header">
                 <div className="apartment-name_container">
-                  <h1>{item.title}</h1>
-                  <h4>{item.location}</h4>
+                  <h1>{apartmentData.title}</h1>
+                  <h4>{apartmentData.location}</h4>
                   {/** Tags section by calling tag componet and passing props.tag_name*/}
                   <ul className="tags-container">
-                    {item.tags.map((tag) => {
+                    {apartmentData.tags.map((tag) => {
                       return <Tag key={tag} tag_name={tag} />
                     })}
                   </ul>
@@ -39,18 +42,18 @@ const Apartment = (props) => {
                 {/** Profil-Avatar section*/}
                 <div className="avatar-wrapper">
                   <div className="avatar-container">
-                    <p>{item.host.name}</p>
+                    <p>{apartmentData.host.name}</p>
                     <div className="avatar">
-                      <img src={item.host.picture} alt="avatar" />
+                      <img src={apartmentData.host.picture} alt="avatar" />
                     </div>
                   </div>
                   <div className="star-container">
                       {/**Create rating-star by checking how many red-star and gray-star we need
                        * then passing props. through the component <Star /> */}
-                      { (item.rating === 5)? <Star  color ="red" n="5"/> : 
+                      { (apartmentData.rating === 5)? <Star  color ="red" n="5"/> : 
                       <Fragment>
-                                 <Star  color ="red" n={item.rating}/>   
-                                 <Star  color ="#E3E3E3" n={5 - parseInt(item.rating)}/>   
+                                 <Star  color ="red" n={apartmentData.rating}/>   
+                                 <Star  color ="#E3E3E3" n={5 - parseInt(apartmentData.rating)}/>   
                       </Fragment>                              }
                   </div>
                 </div>
@@ -58,9 +61,9 @@ const Apartment = (props) => {
               
               {/** Profil-contents section*/}
               <article className='article-container'>
-                      <AboutDropdown name="Desctiption" text={item.description} />
+                      <AboutDropdown name="Desctiption" text={apartmentData.description} />
                       <AboutDropdown name="Équipements" text={
-                        item.equipments.map((list, index)=>{
+                        apartmentData.equipments.map((list, index)=>{
                           return(
                               <p key={list}>{list}</p>
                           )
@@ -69,8 +72,6 @@ const Apartment = (props) => {
 
               </article>
             </section>
-          )
-        })}
       </section>
     </Fragment>
   )
